69332a277400739441748525b388415845aa2691
wick7 <wickersham.craig@gmail.com>
2020-10-28 13:57:11 -0700

    error handling, began cards

--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -1,53 +1,76 @@
 import React, { useReducer, useState, useEffect } from 'react';
-import { Modal, Button, Input, Divider, Row, Col } from 'antd';
+import { Modal, Button, Input, Row, Col } from 'antd';
 import axios from 'axios';
 import './style.css'
 import 'dotenv'
 
 const { Search } = Input;
 
-const Landing = () => {
+const Landing = ({ setCivicData }) => {
 
     const [inputAddress, setInputAddress] = useState('');
     const [modalData, setModalData] = useReducer(
         (state, newState) => ({ ...state, ...newState }),
         {
             ModalText: 'OR',
-            visible: false,
+            visible: true,
             confirmLoading: false,
             lat: 0,
             long: 0
         })
 
     const handleOk = () => {
-
-        let lat;
-        let long;
-
-        navigator.geolocation.getCurrentPosition(function (position) {
-            console.log("Latitude is :", position.coords.latitude);
-            console.log("Longitude is :", position.coords.longitude);
-            lat = position.coords.latitude
-            long = position.coords.longitude
-        })
-        console.log(inputAddress)
-
         const fetchData = async () => {
-            const result = await axios(`https://www.googleapis.com/civicinfo/v2/representatives?address=${inputAddress ? inputAddress : modalData.lat + ',' + modalData.long}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`);
-            console.log(result)
-        };
-
-        setModalData({
-            ModalText: 'Retrieving Data...',
-            confirmLoading: true,
-        });
-        fetchData()
-        setTimeout(() => {
             setModalData({
-                visible: false,
-                confirmLoading: false,
+                ModalText: 'Retrieving Data...',
+                confirmLoading: true,
             });
-        }, 2000);
+
+            await axios(`https://www.googleapis.com/civicinfo/v2/representatives?address=${inputAddress ? inputAddress : modalData.lat + ',' + modalData.long}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`).then((res) => {
+                setTimeout(() => {
+                    setModalData({
+                        visible: false,
+                        confirmLoading: false,
+                    });
+                }, 2000);
+
+                let usSenInfo = res.data.offices[2].divisionId
+                let usHouseInfo = res.data.offices[3].divisionId
+
+                console.log(res.data)
+                console.log(res.data.offices[2])
+                console.log(res.data.offices[3])
+
+
+
+
+                console.log(res.data.divisions[usSenInfo])
+                console.log(res.data.divisions[usHouseInfo])
+
+                console.log(res.data.officials[res.data.offices[2].officialIndices[0]])
+                console.log(res.data.officials[res.data.offices[2].officialIndices[1]])
+                console.log(res.data.officials[res.data.offices[3].officialIndices[0]])
+
+                let civicResults = {
+                    address: res.data.normalizedInput,
+                    upperRepTitle: res.data.offices[2],
+                    lowerRepTitle: res.data.offices[3],
+                    usSenatorOne: res.data.officials[res.data.offices[2].officialIndices[0]],
+                    usSenatorTwo: res.data.officials[res.data.offices[2].officialIndices[1]],
+                    usSenatorsArea: res.data.divisions[usSenInfo],
+                    usHouseRep: res.data.officials[res.data.offices[3].officialIndices[0]],
+                    usHouseArea: res.data.divisions[usHouseInfo],
+                }
+                console.log(civicResults)
+                setCivicData(civicResults)
+            }).catch((err) => {
+                setModalData({
+                    ModalText: 'Sorry, please try either option again. For accurate results please provide a complete address in the input field below.',
+                    confirmLoading: false,
+                });
+            })
+        };
+        fetchData()
     };
 
     const onSearch = (e) => {
@@ -57,7 +80,7 @@ const Landing = () => {
     useEffect(() => {
         console.log('API: ' + process.env.REACT_APP_GOOGLE_API_KEY)
         navigator.geolocation.getCurrentPosition((position) => {
-            setModalData({ visible: true, lat: position.coords.latitude, long: position.coords.longitude })
+            setModalData({ lat: position.coords.latitude, long: position.coords.longitude })
         })
     }, [])
 
--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,22 +1,76 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Card } from 'antd';
 import './style.css';
+import Avatar1 from './avatar1.svg';
 
 const styles = { background: '#0092ff', padding: '8px 0' };
 
-const Layout = ({ isTwo = false }) => {
+const Layout = ({ civicData }) => {
+
+    const usSenRepOneName = civicData && civicData.usSenatorOne.name
+    const usSenRepTwoName = civicData && civicData.usSenatorTwo.name
+    const usSenRepsTitle = civicData && civicData.upperRepTitle.name
+
+    const usSenRepOnePhoto = civicData && civicData.usSenatorOne.photoUrl
+    const usSenRepTwoPhoto = civicData && civicData.usSenatorTwo.photoUrl
+    const usSenRepOneWebLink = civicData && civicData.usSenatorOne.urls
+    const usSenRepTwoWebLink = civicData && civicData.usSenatorTwo.urls
+    const usSenArea = civicData && civicData.usSenatorsArea.name
+
+    const usHouseRepsTitle = civicData && civicData.lowerRepTitle.name
+    const usHouseArea = civicData && civicData.usHouseArea.name
+    const usHouseRepName = civicData && civicData.usHouseRep.name
+    const usHouseRepPhoto = civicData && civicData.usHouseRep.photoUrl
+    const usHouseRepWebLink = civicData && civicData.usHouseRep.urls
+
+
     return (
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={'center'}>
-            <Col className="gutter-row" lg={6} md={9} sm={24} xs={24} >
-                <div style={styles}>col-6</div>
-            </Col>
-            {isTwo ?
-                (<Col className="gutter-row" lg={6} md={9} sm={24} xs={24} >
-                    <div style={styles}>col-6</div>
-                </Col>)
-                : ''
-            }
-        </Row>
+        <>
+            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={'center'}>
+                <Col className="gutter-row" lg={6} md={9} sm={24} xs={24} >
+                    <div style={styles}>
+                        <Card
+                            style={{ width: '100%' }}
+                            cover={<img alt="example" src={usSenRepOnePhoto || Avatar1} />}
+                        >
+                            <h3>{usSenRepOneName}</h3>
+                            {usSenRepsTitle} of {usSenArea}
+
+                            <a href={usSenRepOneWebLink}>Website</a>
+                        </Card>
+                    </div>
+                </Col>
+                <Col className="gutter-row" lg={6} md={9} sm={24} xs={24} >
+                    <div style={styles}>
+                        <Card
+                            style={{ width: '100%' }}
+                            cover={<img alt="example" src={usSenRepTwoPhoto || Avatar1} />}
+                        >
+                            <h3>{usSenRepTwoName}</h3>
+                            {usSenRepsTitle} of {usSenArea}
+
+                            <a href={usSenRepTwoWebLink}>Website</a>
+                        </Card>
+                    </div>
+                </Col>
+            </Row>
+            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={'center'}>
+                <Col className="gutter-row" lg={6} md={9} sm={24} xs={24} >
+                    <div style={styles}>
+                        <Card
+                            style={{ width: '100%' }}
+                            cover={<img alt="example" src={usHouseRepPhoto || Avatar1} />}
+                        >
+                            <h3>{usHouseRepName}</h3>
+                            {usHouseRepsTitle} of {usHouseArea}
+
+                            <a href={usHouseRepWebLink}>Website</a>
+                        </Card>
+                    </div>
+                </Col>
+            </Row>
+
+        </>
     )
 }
 
